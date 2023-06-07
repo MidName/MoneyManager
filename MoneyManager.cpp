0f31670bef0dabd5ef3591ecaacc4e5336e0c01f
MidName <stephenthompson98045@gmail.com>
2023-06-06 19:34:17 -0700

    WIP

--- MoneyManager.cpp
+++ MoneyManager.cpp
@@ -1,20 +1,114 @@
-// MoneyManager.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
+/*
+Create a loading phase that allows for the selection of creating a new file, updating a file, or proceeding with the already written file
+*/
+void startup();
+int selectionScreen();
+void printSelection(int choice);
+std::ofstream outputFile;
+void createNewAccount(std::ofstream outputFile);
 int main()
 {
-    std::cout << "Hello World!\n";
+    int choice;
+    startup();
+    choice = selectionScreen();
+    
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void startup() {
+    std::cout << '\n' << "\t\t\t\t" << "Welcome to V O I D 's Money Manager!" << '\n';
+    std::cout << '\n' << '\t' << "You will be able to calculate how much money you will have left over per month";
+    std::cout << '\n';
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int selectionScreen() {
+    int choice;
+    std::cout << '\n';
+    std::cout << '\t' << "Please make a selection: " << '\n';
+    std::cout << '\t' << "1: Create a new account" << '\n';
+    std::cout << '\t' << "2: Update an account" << '\n';
+    std::cout << '\t' << "3: Continue with previous account" << '\n';
+    std::cout << '\t' << "4: Exit Program" << "\n";
+    std::cout << '\t' << "Choice: ";
+    std::cin >> choice;
+
+    printSelection(choice);
+    switch (choice)
+    {
+    case 1:
+        createNewAccount(outputFile);
+        break;
+    case 2:
+        //UpdateAccount();
+        break;
+    case 3:
+        //Continue();
+        break;
+    case 4:
+        return 1;
+    }
+   
+
+
+
+    return 0;
+}
+
+void printSelection(int choice) {
+    switch (choice)
+    {
+    case 1:
+        std::cout << "You have chosen to create a new account" << '\n';
+        break;
+    case 2:
+        std::cout << "You have chosen to update an existing account" << '\n';
+        break;
+    case 3:
+        std::cout << "You have chosen to proceed with a previously saved account " << '\n';
+        break;
+
+    case 4:
+        std::cout << "You have chosen to Exit the program" << '\n';
+        break;
+    }
+
+}
+
+
+void createNewAccount(std::ofstream outputFile) {
+    double hourlyRate = 0;
+    double monthlyHours = 0;
+    double taxRate = 0;
+    double monthlyExpenses = 0;
+    
+    std::cout << '\n' << '\t' << "Enter the following information:" << '\n';
+    std::cout << "Hourly income rate: ";
+    std::cin >> hourlyRate;
+    std::cout << "Hours worked per month: ";
+    std::cin >> monthlyHours;
+    std::cout << "Estimated tax rate: ";
+    std::cin >> taxRate;
+    std::cout << "Total Monthly expenses";
+    std::cin >> monthlyExpenses;
+    outputFile.open("accountInfo.txt");
+    
+
+    
+    if (outputFile.is_open()) {
+        outputFile << hourlyRate << "\n";
+        outputFile << monthlyHours << "\n";
+        outputFile << taxRate << "\n";
+        outputFile << monthlyExpenses << "\n";
+        outputFile.close();
+    }
+    else {
+        std::cout << '\n' << "Error occurred in opening the file";
+    }
+
+
+}
